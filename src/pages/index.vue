e3798480753a1d0b1f3731166fa82c8b9e82f7da
xiangaxiangaxiang <872632899@qq.com>
2020-03-09 21:37:04 +0800

    canvas

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,29 +1,50 @@
 <template>
   <div class="container">
-    <router-link to="/" class="nav-item">首页</router-link>
-    <router-link to="" class="nav-item">动态</router-link>
-    <router-link to="" class="nav-item">技术</router-link>
-    <router-link to="" class="nav-item">娱乐</router-link>
-    <router-link to="" class="nav-item">远方</router-link>
-    <router-link to="" class="nav-item">关于</router-link>
+    <div class="canvas">
+      <div class="content content--canvas"></div>
+    </div>
+    <nav class="nav">
+      <router-link to="/" class="nav-item">首页</router-link>
+      <router-link to="" class="nav-item">动态</router-link>
+      <router-link to="" class="nav-item">技术</router-link>
+      <router-link to="" class="nav-item">娱乐</router-link>
+      <router-link to="" class="nav-item">远方</router-link>
+      <router-link to="" class="nav-item">关于</router-link>
+    </nav>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop, Watch } from "vue-property-decorator";
+import "common/js/canvas.js";
 @Component
 export default class Index extends Vue {}
 </script>
 <style lang="stylus" scoped>
 @import '../common/stylus/variables'
+@import '../common/stylus/canvas'
 
 .container
   width 100vw
   height 100vh
-  display flex
-  justify-content center
-  align-items center
+  position relative
+  .canvas
+    position absolute
+    left 0
+    right 0
+    width 100%
+    height 100%
+  .nav
+    position absolute
+    left 0
+    right 0
+    width 100%
+    height 100%
+    display flex
+    align-items center
+    justify-content center
+    color white
   .nav-item
     font-size $fs-l
-    margin-left 30px
+    margin-left 40px
 </style>
