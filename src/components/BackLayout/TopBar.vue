a3f539257613c6d306b7a8ad17667b78ec3f1403
xiangaxiangaxiang <42111444+xiangaxiangaxiang@users.noreply.github.com>
2020-06-03 19:49:03 +0800

    Update TopBar.vue

--- src/components/BackLayout/TopBar.vue
+++ src/components/BackLayout/TopBar.vue
@@ -7,7 +7,12 @@
  * @FilePath: \axiang-blog-vue-typescript\src\components\BackLayout\TopBar.vue
 --> 
 <template>
-    <div class=""></div>
+    <header class="layout-header">
+        <div class="bar-title color_blue">
+            <font-awesome-icon :icon="icon" />&nbsp;
+            {{ title }}
+        </div>
+    </header>
 </template>
 
 <script lang="ts">
@@ -17,4 +22,16 @@
     }
 </script>
 <style lang="stylus" scoped>
+    .layout-header
+        height 50px
+        overflow hidden
+        position relative
+        background #fff
+        box-shadow 0 1px 4px rgba(0,21,41,.08)
+        .bar-title
+            float left
+            height 100%
+            line-height 50px
+            min-width 100px
+            margin-left 30px
 </style>
