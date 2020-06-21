9a499e38954b33ac6293f39240414f57267e5e5d
xiangaxiangaxiang <872632899@qq.com>
2020-06-21 21:36:37 +0800

    文章列表

--- src/back/article-lists/index.vue
+++ src/back/article-lists/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-06-02 21:39:56
- * @LastEditTime: 2020-06-19 22:24:43
+ * @LastEditTime: 2020-06-21 21:35:55
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \axiang-blog-vue-typescript\src\back\Article\ArticleManage.vue
@@ -20,6 +20,7 @@
                 type="primary"
                 size="small"
                 icon="el-icon-refresh"
+                @click="getArticles"
             >
                 刷新
             </el-button>
@@ -28,6 +29,9 @@
                 v-model="searchText"
                 class="input-with-select"
                 size="small"
+                @click="searchSubmit"
+                clearable
+                @clear="searchSubmit"
             >
                 <el-button
                     slot="append"
@@ -36,7 +40,7 @@
             </el-input>
             <el-select
                 v-model="labelFilter"
-                placeholder="请选择"
+                placeholder="请选择标签"
                 size="small"
                 class="label-filter"
             >
@@ -48,22 +52,54 @@
                 />
             </el-select>
         </div>
+        <article-list
+            @sort-change="sortChange"
+        />
+        <el-pagination
+            style="margin-top: 1.5rem"
+            background
+            layout="prev, pager, next, total"
+            :total="total"
+            :current-page.sync="currentPage"
+            :page-size="pageSize"
+            @current-change="getArticles"
+        />
     </div>
 </template>
 
 <script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator';
+    import { Component, Vue } from 'vue-property-decorator'
+    import ArticleList from './components/ArticleList.vue'
     @Component({
-        name: 'ArticleLists'
+        name: 'ArticleLists',
+        components: {
+            ArticleList
+        }
     })
     export default class ArticleManage extends Vue {
         private searchText:string = ''
         private labels:string[] = []
         private labelFilter: string = ''
+        // 分页参数
+        private currentPage:number = 1
+        private total:number = 0
+        private pageSize:number = 20
+
+        searchSubmit() {
+            this.currentPage = 1
+            this.getArticles()
+        }
+
+        async getArticles() {}
+
+        sortChange(sort, order) {
+            console.log(sort, order)
+        }
     }
 </script>
 <style lang="stylus" scoped>
     .top-bar
+        margin-bottom 1.5rem
         .input-with-select
             width 30rem
             margin-left 1rem
