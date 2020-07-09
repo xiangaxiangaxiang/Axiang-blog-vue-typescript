c9004244bd72962c49727e2cda34b85b43a35f1f
xiangaxiangaxiang <872632899@qq.com>
2020-07-09 22:02:36 +0800

    首页设计

--- src/frontViews/home/index.vue
+++ src/frontViews/home/index.vue
@@ -1,13 +1,21 @@
 <!--
  * @Author: 翔阿翔阿翔
  * @Date: 2020-05-13 22:28:47
- * @LastEditTime: 2020-05-16 15:43:36
+ * @LastEditTime: 2020-07-09 21:33:33
  * @Description: 博客主页
  * @FilePath: \axiang-blog-vue-typescript\src\front\home\index.vue
  -->
 <template>
     <div class="home-container">
-        <sakura />
+        <sakura class="sakura" />
+        <div class="nav-bar">
+            <div class="img-wrapper">
+                <img
+                    :src="imgSrc"
+                    alt="图片加载失败"
+                >
+            </div>
+        </div>
     </div>
 </template>
 
@@ -19,12 +27,22 @@
             Sakura
         }
     })
-    export default class Home extends Vue {}
+    export default class Home extends Vue {
+        private imgSrc = require('E:/web/project/ftpServer/img/avatar/default_3.jpg')
+    }
 </script>
 <style lang="stylus" scoped>
     .home-container
         width 100vw
         height 100vh
+        position relative
+        .sakura
+            width 100%
+            height 100%
+            position absolute
+            top 0
+            right 0
+            z-index -1000
 </style>
 
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -8,7 +8,7 @@ Vue.use(VueRouter);
 const routes = [{
     path: '/',
     redirect: {
-        name: 'AdminHome'
+        name: 'Home'
     }
 }, ...frontRouter, ...backRouter]
 
