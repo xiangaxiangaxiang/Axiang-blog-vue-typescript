e52fe40d54b921c5e55606cdfa521c7d8dc3bd6b
‘slaochen <872632899@qq.com>
2020-07-14 19:43:06 +0800

    修改顶部导航栏

--- src/components/AccountDropdown/index.vue
+++ src/components/AccountDropdown/index.vue
@@ -28,7 +28,7 @@
             </el-dropdown-menu>
         </el-dropdown>
         <el-dialog
-            :visible="updateAccountDialog"
+            :visible.sync="updateAccountDialog"
             title="修改用户信息"
             width="50rem"
             :modal-append-to-body="false"
@@ -43,6 +43,28 @@
                     prop="nickname"
                 >
                     <el-input v-model="updateForm.nickname" />
+                </el-form-item>
+                <el-form-item
+                    label="昵称"
+                    prop="nickname"
+                >
+                    <el-upload
+                        class="avatar-uploader"
+                        action="https://jsonplaceholder.typicode.com/posts/"
+                        :show-file-list="false"
+                        :before-upload="beforeAvatarUpload"
+                        :on-success="handleAvatarSuccess"
+                    >
+                        <img
+                            v-if="imageUrl"
+                            :src="imageUrl"
+                            class="avatar"
+                        >
+                        <i
+                            v-else
+                            class="el-icon-plus avatar-uploader-icon"
+                        />
+                    </el-upload>
                 </el-form-item>
             </el-form>
             <span slot="footer">
@@ -69,6 +91,7 @@
         private updateForm:object = {
             nickname: ''
         }
+        private imageUrl:string = ''
 
         handleCommand(command) {
             if (command === 'logout') {
@@ -78,10 +101,46 @@
                 this.updateAccountDialog = true
             }
         }
+
+        handleAvatarSuccess(res, file) {
+            this.imageUrl = URL.createObjectURL(file.raw);
+        }
+
+        beforeAvatarUpload(file) {
+            console.log(file)
+            console.log(this.imageUrl)
+            return false
+        }
     }
 </script>
 
 <style lang="stylus" scoped>
     /deep/.el-dropdown-menu__item.is-disabled
         color $blue
+</style>
+
+<style>
+    .avatar-uploader .el-upload {
+        border: 1px dashed #d9d9d9;
+        border-radius: 6px;
+        cursor: pointer;
+        position: relative;
+        overflow: hidden;
+    }
+    .avatar-uploader .el-upload:hover {
+        border-color: #409EFF;
+    }
+    .avatar-uploader-icon {
+        font-size: 28px;
+        color: #8c939d;
+        width: 178px;
+        height: 178px;
+        line-height: 178px;
+        text-align: center;
+    }
+    .avatar {
+        width: 178px;
+        height: 178px;
+        display: block;
+    }
 </style>
