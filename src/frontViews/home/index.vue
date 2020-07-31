179397ce62672c28ff8a623a70a526378f700e11
‘slaochen <872632899@qq.com>
2020-07-31 19:52:52 +0800

    首页

--- src/frontViews/home/index.vue
+++ src/frontViews/home/index.vue
@@ -16,6 +16,24 @@
                         alt="图片加载失败"
                     >
                 </div>
+                <el-row>
+                    <el-col
+                        :span="14"
+                        :offset="5"
+                    >
+                        <ul class="nav-list">
+                            <li
+                                class="nav-item"
+                                v-for="item in navList"
+                                :key="item.path"
+                                @click="pageJump(item.path)"
+                                :class="item.name === '首页' ? 'actice': ''"
+                            >
+                                {{ item.name }}
+                            </li>
+                        </ul>
+                    </el-col>
+                </el-row>
             </div>
         </div>
     </div>
@@ -31,6 +49,37 @@
     })
     export default class Home extends Vue {
         private imgSrc = require('@/assets/avatar.jpg')
+
+        private navList = [{
+            name: '首页',
+            path: '/home'
+        }, {
+            name: '技术',
+            path: '/ariticle/technology'
+        }, {
+            name: '生活',
+            path: '/ariticle/live'
+        }, {
+            name: '梦想',
+            path: '/ariticle/dream'
+        }, {
+            name: '动态',
+            path: '/post'
+        }, {
+            name: 'Github',
+            path: 'https://github.com/xiangaxiangaxiang'
+        }, {
+            name: '其他',
+            path: '/other'
+        }]
+
+        pageJump(path) {
+            if (path.startsWith('http')) {
+                window.open(path,'_blank')
+            } else {
+                this.$router.push(path)
+            }
+        }
     }
 </script>
 <style lang="stylus" scoped>
@@ -62,6 +111,24 @@
                         width 100%
                         height 100%
                         border-radius 50%
+            .nav-list
+                width 80%
+                height 15rem
+                line-height 15rem
+                margin 0 auto
+                .nav-item
+                    display inline-block
+                    width 14%
+                    height 100%
+                    font-size $fs-mx
+                    text-align center
+                    cursor pointer
+                    font-size $fs-l
+                    color white
+                    &:hover
+                        color $light-purple
+                    &.actice
+                        color $orange
 </style>
 
 
