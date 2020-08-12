6921ccafbe3d8c20054c2a0bf1746a0f01c1b831
xiangaxiangaxiang <872632899@qq.com>
2020-08-12 23:06:48 +0800

    文章详情页

--- src/frontViews/article-detail/index.vue
+++ src/frontViews/article-detail/index.vue
@@ -1,25 +1,44 @@
 <template>
-    <div
-        class="article-detail"
-    >
-        <mavon-editor
-            v-model="markdown"
-            style="height: 100%;background: rgba(0,0,0,0.1);"
-            font-size="2rem"
-            :tab-size="4"
-            ref="md"
-            default-open="preview"
-            :toolbars-flag="false"
-            :navigation="true"
-            :editable="false"
-            :subfield="false"
-        />
-    </div>
+    <el-row class="detail-container">
+        <el-col
+            :span="2"
+            class="left-side"
+        >
+            <div
+                class="item"
+                :class="likeStatus ? 'like': 'dislike'"
+                @click="handleLike"
+            >
+                <font-awesome-icon icon="heart" />
+            </div>
+            <div class="item comments">
+                <font-awesome-icon icon="comments" />
+            </div>
+        </el-col>
+        <el-col
+            :span="14"
+            style="height: 100%;"
+        >
+            <el-scrollbar class="scrollbar">
+                <h1 class="article-title">
+                    {{ articleTitle }}
+                </h1>
+                <div
+                    class="article-detail markdown-body"
+                    v-html="articleHtml"
+                />
+            </el-scrollbar>
+        </el-col>
+        <el-col :span="6">
+            lalala
+        </el-col>
+    </el-row>
 </template>
 
 <script lang="ts">
     import { Component, Vue, Prop } from 'vue-property-decorator'
     import { getArticleDetailApi } from '@/api/front/article'
+    import 'mavon-editor/dist/css/index.css'
     @Component({
         name: 'ArticleDetail'
     })
@@ -27,6 +46,8 @@
         @Prop({default: ''}) articleId!:string
         private articleHtml:string = ''
         private markdown:string = ''
+        private articleTitle:string = ''
+        private likeStatus:boolean = false
 
         mounted() {
             this.getArticleDetail()
@@ -40,9 +61,57 @@
             if (res && res.status === 0) {
                 this.articleHtml = res.data.html
                 this.markdown = res.data.markdown
+                this.articleTitle = res.data.title
             }
+        }
+
+        handleLike() {
+            this.likeStatus = !this.likeStatus
         }
     }
 </script>
-<style lang="stylus" scoped>
+
+<style scoped lang="stylus">
+    .detail-container
+        width 100%
+        height $contentHeight
+        padding-top 1.5rem
+        .left-side
+            height 100%
+            display flex
+            flex-direction column
+            justify-content center
+            align-items center
+            .item
+                width 4rem
+                height 4rem
+                line-height 4rem
+                border-radius 50%
+                background rgba(255, 255, 255, .8)
+                margin-top 2rem
+                text-align center
+                font-size $fs-ss
+                cursor pointer
+                user-select none
+                &.comments
+                    color $blue
+                &.like
+                    color $red
+                &.dislike
+                    color $second-text
+        .scrollbar
+            width 100%
+            height 100%
+            padding 0 2rem
+            .article-title
+                width 100%
+                height 8rem
+                line-height 8rem
+                font-weight bold
+                font-size $article-title
+                color $orange
+            .article-detail
+                margin-top 3rem
+                padding-bottom 5rem
+                color $blue
 </style>
