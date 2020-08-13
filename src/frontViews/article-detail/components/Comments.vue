f846bde6551260b6123ff41e299e83eb6323fbda
xiangaxiangaxiang <872632899@qq.com>
2020-08-13 23:10:17 +0800

    点赞、回到顶部功能实现

---
+++ src/frontViews/article-detail/components/Comments.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="comment-container">
+        <div
+            v-for="item in 30"
+            :key="item"
+            class="list"
+        >
+            {{ item }}
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+    @Component({
+        name: 'Comments'
+    })
+    export default class Comments extends Vue {
+    }
+</script>
+<style lang="stylus" scoped>
+    .list
+        width 100%
+        height 3rem
+        line-height 3rem
+</style>
--- src/frontViews/article-detail/index.vue
+++ src/frontViews/article-detail/index.vue
@@ -1,5 +1,7 @@
 <template>
-    <el-row class="detail-container">
+    <el-row
+        class="detail-container"
+    >
         <el-col
             :span="2"
             class="left-side"
@@ -8,18 +10,33 @@
                 class="item"
                 :class="likeStatus ? 'like': 'dislike'"
                 @click="handleLike"
+                :title="likeStatus ? '取消点赞': '点赞'"
             >
                 <font-awesome-icon icon="heart" />
             </div>
-            <div class="item comments">
+            <div
+                class="item comments"
+                title="查看评论"
+            >
                 <font-awesome-icon icon="comments" />
+            </div>
+            <div
+                class="item back-to-top"
+                :class="{show: showBackToTop}"
+                title="回到顶部"
+                @click="backToTop"
+            >
+                <font-awesome-icon icon="caret-up" />
             </div>
         </el-col>
         <el-col
             :span="14"
             style="height: 100%;"
         >
-            <el-scrollbar class="scrollbar">
+            <el-scrollbar
+                class="scrollbar"
+                ref="articleScrollbar"
+            >
                 <h1 class="article-title">
                     {{ articleTitle }}
                 </h1>
@@ -27,6 +44,7 @@
                     class="article-detail markdown-body"
                     v-html="articleHtml"
                 />
+                <comments />
             </el-scrollbar>
         </el-col>
         <el-col :span="6">
@@ -38,9 +56,13 @@
 <script lang="ts">
     import { Component, Vue, Prop } from 'vue-property-decorator'
     import { getArticleDetailApi } from '@/api/front/article'
-    import 'mavon-editor/dist/css/index.css'
+    import Comments from './components/Comments.vue'
+    import { debounce } from 'lodash'
     @Component({
-        name: 'ArticleDetail'
+        name: 'ArticleDetail',
+        components: {
+            Comments
+        }
     })
     export default class ArticleDetail extends Vue {
         @Prop({default: ''}) articleId!:string
@@ -48,9 +70,33 @@
         private markdown:string = ''
         private articleTitle:string = ''
         private likeStatus:boolean = false
+        private showBackToTop:boolean = false
 
         mounted() {
             this.getArticleDetail()
+            const scrollbar = this.$refs.articleScrollbar
+            // eslint-disable-next-line
+            const wrap = ( scrollbar as HTMLFormElement ).$refs['wrap']
+            wrap.addEventListener('scroll', debounce(this.handleScroll, 100))
+        }
+
+        handleScroll(e) {
+            if (e.target.scrollTop > 0) {
+                this.showBackToTop = true
+            } else {
+                this.showBackToTop = false
+            }
+        }
+
+        backToTop() {
+            const scrollbar = this.$refs.articleScrollbar
+            // eslint-disable-next-line
+            const wrap = ( scrollbar as HTMLFormElement ).$refs['wrap']
+            console.log(wrap.animate({scrollTop: 0, duration: 1000}))
+            wrap.animate({
+                scrollTop: 0,
+                duration: 1000
+            })
         }
 
         async getArticleDetail() {
@@ -99,6 +145,13 @@
                     color $red
                 &.dislike
                     color $second-text
+                &.back-to-top
+                    color $blue
+                    font-size $fs-l
+                    opacity 0
+                    transition .3s
+                &.show
+                    opacity 1
         .scrollbar
             width 100%
             height 100%
