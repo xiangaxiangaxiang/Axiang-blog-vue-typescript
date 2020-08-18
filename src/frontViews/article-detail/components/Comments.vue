508c9656e989c6e123d8b00f33bb9d82862c593d
xiangaxiangaxiang <872632899@qq.com>
2020-08-18 23:04:26 +0800

    '评论功能'

--- src/frontViews/article-detail/components/Comments.vue
+++ src/frontViews/article-detail/components/Comments.vue
@@ -1,17 +1,54 @@
 <template>
     <div class="comment-container">
-        <picker
-            :include="['people']"
-            :show-search="false"
-            :show-preview="false"
-            :show-categories="false"
-            @select="addEmoji"
-        />
+        <div class="comment-input">
+            <div class="avatar">
+                <img
+                    v-if="avatar"
+                    :src="avatar"
+                    alt="加载错误"
+                >
+            </div>
+            <el-input
+                v-model="comment"
+                class="comment"
+            />
+            <div
+                class="action-box clearfix"
+                v-show="showAction"
+            >
+                <div
+                    class="emoji-btn"
+                    @click="showEmojiSelect=!showEmojiSelect"
+                >
+                    <span>
+                        表情
+                    </span>
+                    <font-awesome-icon icon="smile" />
+                    <picker
+                        :include="['people']"
+                        :show-search="false"
+                        :show-preview="false"
+                        :show-categories="false"
+                        @select="addEmoji"
+                        v-show="showEmojiSelect"
+                        @click.stop
+                    />
+                </div>
+                <div class="submit-button">
+                    <el-button
+                        type="primary"
+                        size="small"
+                    >
+                        评论
+                    </el-button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator'
+    import { Component, Vue, Watch } from 'vue-property-decorator'
     import { Picker } from 'emoji-mart-vue'
     @Component({
         name: 'Comments',
@@ -20,14 +57,65 @@
         }
     })
     export default class Comments extends Vue {
+        private avatar:string = sessionStorage.getItem('avatar') as string
+        private comment:string = ''
+        private showAction:boolean = true
+        private showEmojiSelect:boolean = false
+
+        @Watch('showEmojiSelect')
+        changeShowEmojiSelect(newValue) {
+            console.log(newValue)
+            if (newValue) {
+                const that = this
+                window.addEventListener('click', function(e) {
+                    console.log(e.target)
+                    that.showEmojiSelect = false
+                } ,false)
+            }
+        }
+
         addEmoji(e) {
-            console.log(e.native.toString())
+            this.comment += e.native
+            this.showEmojiSelect = false
         }
     }
 </script>
 <style lang="stylus" scoped>
-    .list
-        width 100%
-        height 3rem
-        line-height 3rem
+    .comment-container
+        padding 2rem
+        .comment-input
+            width 100%
+            padding 1rem
+            margin-bottom 1rem
+            background $line-grey
+            .avatar
+                width 4rem
+                height 4rem
+                border-radius 50%
+                border 1px solid $blue
+                display inline-block
+                .img
+                    width 100%
+                    height 100%
+            .comment
+                vertical-align top
+                width calc(100% - 5rem)
+                margin-left 1rem
+                /deep/.el-input__inner
+                    background rgba(255, 255, 255, .5) !important
+            .action-box
+                width 100%
+                height 3rem
+                margin-top 1rem
+                padding-left 5rem
+                .emoji-btn
+                    width 5rem
+                    height 100%
+                    line-height 3rem
+                    color $blue
+                    font-size $fs-ss
+                    float left
+                    cursor pointer
+                .submit-button
+                    float right
 </style>
